5c3ba0d7bdeb304b98761172f8cacc70e2b28d00
parevalo <parevalo@bu.edu>
2016-03-16 15:23:24 -0400

    Fix indentation and change numbe rof jobs

--- Classify.sh
+++ Classify.sh
@@ -5,10 +5,10 @@ cd /projectnb/landsat/projects/Colombia/images/007058/Results/FIT1/logs
 cfg_path=/projectnb/landsat/projects/Colombia/images/007058/Results/FIT1/758_FIT1.yaml
 algo_path=/projectnb/landsat/projects/Colombia/classifiers/859_trainRF1.pkl
 
-njob=400
+njob=200
 
 for job in $(seq 1 $njob); do
     qsub -j y -V -N yclass_$job -b y \
 	  yatsm -v classify $cfg_path $algo_path $job $njob
-  done
+done
 
