279201dcb77d6d79d609799e3911a2b0c354765a
parevalo <parevalo@bu.edu>
2016-03-22 18:55:26 -0400

    Create folders for individual and multi scripts

---
+++ single_scene/README.md
@@ -0,0 +1,2 @@
+# workflow
+Repository to include all the basic scripts needed to run all steps of yatsm for each of the scenes
---
+++ single_scene/Yatsm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /projectnb/landsat/projects/Colombia/images/007058/Results/FIT1/logs
+yconfig=/projectnb/landsat/projects/Colombia/images/007058/Results/FIT1/758_FIT1.yaml
+
+njob=400
+
+for job in $(seq 1 $njob); do
+    qsub -j y -V -N yatsm_$job -b y \
+	 yatsm -v line --check_cache --resume $yconfig $job $njob
+done
+
