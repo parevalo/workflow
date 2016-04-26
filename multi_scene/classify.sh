3092487ec8b2f14fc1e3c612182256c8cd2bbb3e
parevalo <parevalo@bu.edu>
2016-04-25 21:35:36 -0400

    Include all 20 scenes

--- multi_scene/classify.sh
+++ multi_scene/classify.sh
@@ -6,14 +6,16 @@
 
 # List of scenes to be processed
 
-scn_list="005058 006058"
+scn_list="004057 004058 004061 004062 005057 005058 005059 005060 \
+          005061 006058 006059 006060 006061 007058 007059 007060 \
+          008058 008059 008060 009059"
 
 # General setting: path to template, root dir, etc
 
 yconfig=/projectnb/landsat/projects/Colombia/workflow/multi_scene/yatsm_config.yaml
 algopath=/projectnb/landsat/projects/Colombia/classifiers
 export ROOTDIR=/projectnb/landsat/projects/Colombia/images
-njob=200
+njob=400
 
 # Iterate over scenes
 
@@ -24,33 +26,34 @@ for s in $scn_list; do
 
     # Export all relevant variables for the yaml file
     export INPUT=$ROOTDIR/$s/$pt$rw"_input.csv"
-    export RESULTS=$ROOTDIR/$s/Results/FIT1/TSR
-    export CONFIG=$ROOTDIR/$s/Results/FIT1
+    export RESULTS=$ROOTDIR/$s/Results/M1/TSR
     export IMG=$ROOTDIR/$s/images
-    export TRAINING=$ROOTDIR/$s/images/Training1.tif
+    #export TRAINING=$ROOTDIR/$s/images/Training1.tif
     
-    # Test for scenes and change the start and end train date accordingly   
-    # THIS SECTION IS PROBABLY UNNECESSARY 
-    if [ $s = "005058" ]; then 
-        export TRAINSTART="2000-078"
-        export TRAINEND="2001-032"
-    elif [ $s = "006058" ]; then 
-        export TRAINSTART="2001-031"
-        export TRAINEND="2003-013"
-    else
-        echo "Date in conditions don't match those in the list"
-        exit
-    fi
+    # Change the start and end train date accordingly if using diff. training
+    # 
+    #if [ $s = "005058" ]; then 
+    #    export TRAINSTART="2000-078"
+    #    export TRAINEND="2001-032"
+    #elif [ $s = "006058" ]; then 
+    #    export TRAINSTART="2001-031"
+    #    export TRAINEND="2003-013"
+    #else
+    #    echo "Date in conditions don't match those in the list"
+    #    exit
+    #fi
 
     # CD to classifiers folder
-    cd /projectnb/landsat/projects/Colombia/logs/$pt$rw
+    cd /projectnb/landsat/projects/Colombia/logs/$pt$rw/M1
     
 
     # Run classification, verify algorithm being used
     for job in $(seq 1 $njob); do
         qsub -j y -V -N class$pt$rw"_"$job -b y \
          yatsm -v classify $yconfig $algopath/mergedtrain_859-658-558.pkl $job $njob 
-         # yatsm -v classify $yconfig $algopath/$pt$rw"_trainRF1.pkl" $job $njob 
         
     done
+    
+    # For debugging purposes
+    #yatsm -v classify $yconfig $algopath/mergedtrain_859-658-558.pkl 1 $njob 
 done 
--- multi_scene/map.sh
+++ multi_scene/map.sh
@@ -10,7 +10,9 @@
 
 # List of scenes to be processed
 
-scn_list="005058 006058"
+scn_list="004057 004058 004061 004062 005057 005058 005059 005060 \
+          005061 006058 006059 006060 006061 007058 007059 007060 \
+          008058 008059 008060 009059"
 
 # General setting: path to template, root dir, etc
 
@@ -26,7 +28,7 @@ for s in $scn_list; do
     # Set scene, TS(images) and results paths for the map script 
     scn_path=$rootdir/$s
     ts_path=$scn_path/images
-    res_path=$scn_path/Results/FIT1/TSR
+    res_path=$scn_path/Results/M1/TSR
 
     # Find example image
     img=$(find $ts_path -maxdepth 1 -type d -name "*LE7*" | head -1 )
@@ -34,14 +36,15 @@ for s in $scn_list; do
     
     # Set variables for map script
     img_path=$ts_path/$example_img/$example_img"_stack" 
-    class_date=2001-01-31
+    class_day=-01-01
  
     # CD to Class folder
-    cd /projectnb/landsat/projects/Colombia/images/$s/Results/FIT1/Class
+    cd /projectnb/landsat/projects/Colombia/images/$s/Results/M1/Class
 
-    # Run map script
-    qsub -j y -V -N map_$pt$rw -b y \
-     yatsm -v map --root $ts_path --result $res_path --image $img_path\
-      class $class_date Class3_"$class_date".tif    
-    
+    # Run map script for multiple dates
+    for yr in $(seq -w 0 15); do    
+        qsub -j y -V -N map_$pt$rw"-"$yr -b y \
+         yatsm -v map --root $ts_path --result $res_path --image $img_path\
+          class 20$yr$class_day ClassM1A_20$yr$class_date".tif"
+    done
 done 
