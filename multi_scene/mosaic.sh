f2bd3943c18637dc9e426accf719b59aba2a6351
parevalo <parevalo@bu.edu>
2016-04-26 20:49:32 -0400

    Changed EPSG to MAGNA-SIRGAS

--- multi_scene/mosaic.sh
+++ multi_scene/mosaic.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Basic version of mosaicking script for a single date
+# Basic version of mosaicking script for a single date. A more advanced version
+# will be needed to specify the order of the rasters according the the scenes
+# with the highest number of images
+# MAGNA SIRGAS is EPSG:4686
+# UTM18-19N are EPSG:32618-32619
 
+cd /projectnb/landsat/projects/Colombia/Mosaics/M1A
 
-qsub -V -j y -b y gdalwarp -co BIGTIFF=YES -wt Byte -t_srs EPSG:32619 -srcnodata 0  \
- "/projectnb/landsat/projects/Colombia/images/*/Results/M1/Class/ClassM1A_2000.tif" 2000.tif
+#module load gdal/1.10.0, not working!
+
+qsub -pe omp 4 -V -j y -b y gdalwarp -multi -co NBITS=4 -wt Byte -t_srs EPSG:4686 -srcnodata 0  \
+ "/projectnb/landsat/projects/Colombia/images/*/Results/M1/Class/ClassM1A_2008.tif" 2008_MAGNA_SIRGAS.tif
+
