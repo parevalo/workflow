db28cc84f602aa9e6af63ef78b88c385cca95894
parevalo <parevalo@bu.edu>
2016-06-01 17:04:52 -0400

    Added script to reproject to UTM19, works correctly with the expected few modified pixels bc of resampling

--- multi_scene/4_postprocess/reproject_mosaic.sh
+++ multi_scene/4_postprocess/reproject_mosaic.sh
@@ -11,12 +11,14 @@ export GDAL_DATA=/usr3/graduate/parevalo/miniconda2/envs/GDAL_ENV/share/gdal
 # cd to the corresponding class folder
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
     
-# Submit the reprojection job
+# Project to UTM18N 
+#qsub -V -b y -j y -N reproj_UTM18N \
+#gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
+#-te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
+#-t_srs EPSG:32618 -tr 30 -30 eastUTM19_2016.tif eastUTM18tapnewextent.tif
+
+# (Re)project to UTM19N
 qsub -V -b y -j y -N reproj_mUTM19N \
 gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
--te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
--t_srs EPSG:32618 -tr 30 -30 eastUTM19_2016.tif eastUTM18tapnewextent.tif
-
-#qsub -V -b y -j y -N reproj_mUTM19N \
-#gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
-#-t_srs EPSG:32619 -tr 30 -30 eastUTM18.tif east_reprojUTM19.tif
+-te $(gdal_extent eastUTM19_2016.tif) -te_srs EPSG:32619 \
+-t_srs EPSG:32619 -tr 30 -30 eastUTM18tapnewextent.tif east_reprojUTM19.tif
