863dac2352b056cc8a04d7a54f0385934f4a0920
parevalo <parevalo@bu.edu>
2016-06-01 17:36:49 -0400

    - Removed unsetnodata because the new merging script makes that step unnecesary - Temporary changes to test options

--- multi_scene/6_stratification/clip_mosaics.sh
+++ multi_scene/6_stratification/clip_mosaics.sh
@@ -1,24 +1,19 @@
 #!/bin/bash -l
 
-# Script to clip mosaic to Amazon boundary. Requires GDAL >=2.1
+# Script to clip mosaic to Amazon boundary. 
 
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
-ps=0.000270099 #Pixel size in degrees in case it's needed
 limit=/projectnb/landsat/projects/Colombia/vector/amazon_boundary.shp
 
-# Unset NoData and clip
+#Do clipping
 
-for yr in $(seq -w 01 01); do
-    
-    # Unset NoData so that we can operate over areas with value of 0
-    gdal_edit.py -unsetnodata 20$yr"_final_UTM18N.tif"
+for yr in $(seq -w 16 16); do
     
     # Clip    
     qsub -j y -V -N clip$yr -b y \
      gdalwarp -cutline $limit -cl amazon_boundary \
-      -s_srs EPSG:32618 -t_srs EPSG:32618 \
-      -ot Byte -co COMPRESS=PACKBITS -overwrite 20$yr"_final.tif" \
-       20$yr"_final_UTM18N_crop.tif"
+      -ot Byte -co COMPRESS=PACKBITS -overwrite finalmergeexample.tif \
+      20$yr"_final_UTM18N_crop.tif"
 
 done
 
