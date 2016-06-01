66d20f6cbf6a269b2141765fa5eb4be82f4fb671
parevalo <parevalo@bu.edu>
2016-06-01 18:22:57 -0400

    Updated version to include year loop and updated names

--- multi_scene/5_mosaic/merge_zones.sh
+++ multi_scene/5_mosaic/merge_zones.sh
@@ -2,13 +2,12 @@
 # Merge west and east zones, both in UTM18
 
 export GDAL_DATA=/usr3/graduate/parevalo/miniconda2/envs/GDAL_ENV/share/gdal
-#module load python/2.7.5_nopath
-#module load gdal/1.11.1
 
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
 
-qsub -V -N mergezones -j y -b y \
- gdal_merge.py -o finalmergeexample.tif -co COMPRESS=PACKBITS -co NBITS=4 \
- -ot Byte -n 0 -v eastUTM18tapnewextent.tif westUTM18_2016.tif
-
+for yr in $(seq -w 01 16); do
+    qsub -V -N mrgezones_$yr -j y -b y \
+     gdal_merge.py -o 20$yr"_final.tif" -co COMPRESS=PACKBITS -co NBITS=4 \
+     -ot Byte -n 0 -v eastUTM18_20$yr".tif" westUTM18_20$yr".tif"
+done
 
