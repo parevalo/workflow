00e753b0f0cdd0db3f8aec0b74ba8ddaca44405d
parevalo <parevalo@bu.edu>
2016-06-16 10:39:26 -0400

    Latest minor changes

--- multi_scene/5_mosaic/reproject_mosaic.sh
+++ multi_scene/5_mosaic/reproject_mosaic.sh
@@ -12,15 +12,16 @@ cd /projectnb/landsat/projects/Colombia/Mosaics/M3
 # Project to UTM18N. Extent calculated manually in order to "snap" the new
 # raster to the west zone grid. 
 
-for yr in $(seq -w 01 16); do
-    qsub -V -b y -j y -N repr18N_$yr \
-    gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
-    -te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
-    -t_srs EPSG:32618 -tr 30 -30 eastUTM19_20$yr".tif" eastUTM18_20$yr".tif"
-done
+#for yr in $(seq -w 01 16); do
+#    qsub -V -b y -j y -N repr18N_$yr \
+#    gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
+#    -te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
+#    -t_srs EPSG:32618 -tr 30 -30 eastUTM19_20$yr".tif" eastUTM18_20$yr".tif"
+#done
 
-# (Re)project to UTM19N (in case it's needed later)
-#qsub -V -b y -j y -N reproj_mUTM19N \
-#gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
-#-te $(gdal_extent eastUTM19_2016.tif) -te_srs EPSG:32619 \
-#-t_srs EPSG:32619 -tr 30 -30 eastUTM18tapnewextent.tif east_reprojUTM19.tif
+# (Re)project to UTM19N (needed to reproject strata and samples)
+qsub -V -b y -j y -N reproj_UTM19N \
+gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
+-te $(gdal_extent eastUTM19_2016.tif) -te_srs EPSG:32619 \
+-t_srs EPSG:32619 -tr 30 -30 sample_may2016_UTM18N.tif sample_may2016_UTM19N.tif
+
