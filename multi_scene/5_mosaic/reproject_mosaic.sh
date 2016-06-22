000ce95311edc66598c89097dcc71481a9a3ea5e
parevalo <parevalo@bu.edu>
2016-06-22 10:48:36 -0400

    Add new additional sample and remove overwrite for safety

--- multi_scene/5_mosaic/reproject_mosaic.sh
+++ multi_scene/5_mosaic/reproject_mosaic.sh
@@ -21,7 +21,8 @@ cd /projectnb/landsat/projects/Colombia/Mosaics/M3
 
 # (Re)project to UTM19N (needed to reproject strata and samples)
 qsub -V -b y -j y -N reproj_UTM19N \
-gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte -overwrite \
+gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte \
 -te $(gdal_extent eastUTM19_2016.tif) -te_srs EPSG:32619 \
--t_srs EPSG:32619 -tr 30 -30 sample_may2016_UTM18N.tif sample_may2016_UTM19N.tif
+-t_srs EPSG:32619 -tr 30 -30 additional_forest_sample_UTM18N.tif additional_forest_sample_UTM19N.tif
+#-t_srs EPSG:32619 -tr 30 -30 sample_may2016_UTM18N.tif sample_may2016_UTM19N.tif
 
