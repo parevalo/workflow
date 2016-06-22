a342ac85431be6f748f102488c10790c0ac87c0e
parevalo <parevalo@bu.edu>
2016-06-22 10:49:15 -0400

    Add new additional forest sample

--- multi_scene/6_stratification/submit_sample.sh
+++ multi_scene/6_stratification/submit_sample.sh
@@ -8,8 +8,15 @@
 
 fpath=/projectnb/landsat/projects/Colombia/Mosaics/M3
 
-python sample_map.py -v --size 850 \
---allocation "50 200 75 50 75 50 50 50 50 50 50 50 50" \
---mask "15 255" --ndv 255 --vector $fpath/sample_may2016.shp --seed_val 10000 \
-stratified $fpath/strata_01_16_UTM18N.tif
+#python sample_map.py -v --size 850 \
+#--allocation "50 200 75 50 75 50 50 50 50 50 50 50 50" \
+#--mask "15 255" --ndv 255 --vector $fpath/sample_may2016.shp --seed_val 10000 \
+#stratified $fpath/strata_01_16_UTM18N.tif
+
+# Additional forest sample
+
+python sample_map.py -v --size 200 \
+--allocation "0 200 0 0 0 0 0 0 0 0 0 0 0" \
+--mask "15 255" --ndv 255 --vector $fpath/additional_forest_sample_UTM18N.shp \
+--seed_val 10001 stratified $fpath/strata_01_16_UTM18N.tif
 
