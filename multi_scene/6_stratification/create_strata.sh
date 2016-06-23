8339a680918b2749172b0c731d3412e0aed63384
parevalo <parevalo@bu.edu>
2016-06-23 15:36:52 -0400

    Changed to reflect most recent changes to stratification

--- multi_scene/6_stratification/create_strata.sh
+++ multi_scene/6_stratification/create_strata.sh
@@ -11,16 +11,16 @@ initial=2001_final_crop.tif
 for yr in $(seq -w 16 16); do
     qsub -j y -V -N strata_$yr -b y \
      gdal_calc.py -A $initial -B 20$yr"_final_crop.tif" \
-      --outfile=strata_01_$yr"_UTM18N.tif" \
+      --outfile=final_strata_01_$yr"_UTM18N.tif" \
       --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*2 +' \
               'logical_and(A == 3, B==3)*3 + logical_and(A == 4, B==4)*4 +' \
               'logical_and(A == 5, B==5)*5 + logical_and(A == 5, B==1)*5 +' \
               'logical_and(A == 6, B==6)*6 +' \
               'logical_and(A == 7, B==7)*3 + logical_and(A == 1, B==4)*8 +' \
               'logical_and(A == 1, B==5)*9 +' \
-              'logical_and(A == 1, logical_or(B==2, B==3))*10 +' \
-              'logical_and(A == 1, logical_or(B==6, B==7))*10 +' \
-              'logical_and(A == 1, B == 0)*10 +' \
+              'logical_and(A == 1, logical_or(B==2, B==3))*0 +' \
+              'logical_and(A == 1, logical_or(B==6, B==7))*0 +' \
+              'logical_and(A == 1, B == 0)*8 +' \
               'logical_and(A == 4, B==5)*11 +' \
               'logical_and(logical_or(A == 2, A==3), B==5)*11 +' \
               'logical_and(logical_or(A == 6, A==7), B==5)*11 +' \
