2a3110705b9e70cf6dd5ddb16bf4c3e9b60cd5d3
parevalo <parevalo@bu.edu>
2016-06-27 17:39:09 -0400

    Non working version

--- multi_scene/7_poststratification/area_estimation.py
+++ multi_scene/7_poststratification/area_estimation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-"""Area calculation with standard errors
+"""Area calculation with standard errors. Assumes input table has row and 
+   column labels.   
 
 Usage:
     estimate_area.py <error matrix csv)
 """
+#TODO:
+#- Add back multiple options for specifying row columns/labels
+#- Add options to determine what to export, e.g. area proportions, CI, etc
+
 from docopt import docopt
 
 import pandas as pd
@@ -17,16 +22,31 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 def read_csv(csvfile):
     """
-    Reads CSV file with error matrix as numpy array
+    Reads CSV file with error matrix as pandas dataframe
     """
     # Read in file
-    with open(csvfile, 'rb') as f:
-        csvreader = csv.reader(f)
-        if header:
-            csvreader.next()
-        errormatrix = { int(row[0]) : int(row[1]) for row in csvreader }
+    df = pd.read_csv(csvfile, sep=',', header=0, index_col=0)
+
+    return df
 
 
-return lut
+def calculate_areas(df):
+    # Convert to numpy array to make it easier
+    table = df.values
 
+    # Get number of rows, assumed to be number of classes
+    numclasses = table.shape[0]
 
+    # Calculate terms of the equation. 
+    S = np.zeros((numclasses))
+    ni = table[:, 12]
+    w = table[:, 15]
+    p = w * table[:,0:12] / ni
+   
+    for i in range(numclasses):
+        # Iterate over columns (Attepmt again using removing p line below and indexing directly)
+        p = w * table[:, i] / ni
+        np.sqrt(np.sum((w * p - p**2) / (ni  - 1)))
+
+    print S
+    return S
