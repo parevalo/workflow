43e4fda9c8148f3bbfdc082a50d975cea19e04bb
parevalo <parevalo@bu.edu>
2016-06-29 11:42:27 -0400

    modified to created rasters/polygon based on IDs, in order to make it possible to join attribute table back

--- multi_scene/7_poststratification/reproject_samples.sh
+++ multi_scene/7_poststratification/reproject_samples.sh
@@ -9,25 +9,44 @@ export GDAL_DATA=/usr3/graduate/parevalo/miniconda2/envs/GDAL_ENV/share/gdal
 # cd to the corresponding class folder
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
 
-# Rasterize east sample
+# Rasterize east samples (original and additional)
 qsub -j y -b y -V -N rast_samp1 \
-    gdal_rasterize -a strata -a_nodata 255 \
+    gdal_rasterize -a ID -a_nodata 255 \
     -te $(gdal_extent eastUTM19_2016.tif) -tr 30 30 -ot Byte \
     -co COMPRESS=PACKBITS sample_east_UTM19N_ID_PR_ZONE.shp \
     sample_east_UTM19N_ID_PR_ZONE.tif
 
-# Reproject east sample to UTM18
-qsub -j y -b y -V -N repr_sample -hold_jid rast_samp1 \
+qsub -j y -b y -V -N rast_samp2 \
+    gdal_rasterize -a ID -a_nodata 255 \
+    -te $(gdal_extent eastUTM19_2016.tif) -tr 30 30 -ot Byte \
+    -co COMPRESS=PACKBITS additional_forest_sample_east_UTM19N_ID_PR_ZONE.shp \
+    additional_forest_sample_east_UTM19N_ID_PR_ZONE.tif
+
+# Reproject east samples to UTM18
+qsub -j y -b y -V -N repr_sample1 -hold_jid rast_samp1 \
     gdalwarp -co COMPRESS=PACKBITS -wt Byte -overwrite \
     -te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
     -t_srs EPSG:32618 -tr 30 -30 sample_east_UTM19N_ID_PR_ZONE.tif \
     sample_east_UTM18N_ID_PR_ZONE.tif
 
-# Poligonize that file
-qsub -j y -b y -V -N pol_sample -hold_jid repr_sample \
-    gdal_polygonize.py sample_east_UTM18N_ID_PR_ZONE.tif -f '"ESRI Shapefile"' \
-    sample_east_UTM18N_ID_PR_ZONE.shp sample_east_UTM18N_ID_PR_ZONE strata
+qsub -j y -b y -V -N repr_sample2 -hold_jid rast_samp2 \
+    gdalwarp -co COMPRESS=PACKBITS -wt Byte -overwrite \
+    -te 725295 -429765 1506435 595185 -te_srs EPSG:32618 \
+    -t_srs EPSG:32618 -tr 30 -30 additional_forest_sample_east_UTM19N_ID_PR_ZONE.tif \
+    additional_forest_sample_east_UTM18N_ID_PR_ZONE.tif
 
-# Then manually merge the shapefiles from the two zones...
+
+# Poligonize those files
+qsub -j y -b y -V -N pol_sample -hold_jid repr_sample1 \
+    gdal_polygonize.py sample_east_UTM18N_ID_PR_ZONE.tif -f '"ESRI Shapefile"' \
+    sample_east_UTM18N_ID_PR_ZONE.shp sample_east_UTM18N_ID_PR_ZONE ID
+
+qsub -j y -b y -V -N pol_sample -hold_jid repr_sample2 \
+    gdal_polygonize.py additional_forest_sample_east_UTM18N_ID_PR_ZONE.tif -f '"ESRI Shapefile"' \
+    additional_forest_sample_east_UTM18N_ID_PR_ZONE.shp additional_forest_sample_east_UTM18N_ID_PR_ZONE ID
+
+
+# Then manually join back the attribute table from the east zone for the two
+# shapefiles and merge the shapefiles from the two zones...
 
 
