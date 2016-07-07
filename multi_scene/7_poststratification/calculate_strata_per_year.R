e6d539a3a0ded645a2e5862e644fe78e0d203022
parevalo <parevalo@bu.edu>
2016-07-07 17:42:24 -0400

    Multiple changes, issues with variances and standard errors still present

--- multi_scene/7_poststratification/calculate_strata_per_year.R
+++ multi_scene/7_poststratification/calculate_strata_per_year.R
@@ -1,23 +1,22 @@
-## This script takes the sample shapefile and assigns the proper strata for each year, depending
-## on when the model break happened. The way it's written now it only works for the FIRST change, 
-## so the input shapefile must be that with samples with only stable or 1 change pixels
-## The code is not very efficient (too many loops and conditionals) but it does the job
+### This script has three main sections:
+### 1) Read reference strata shapefile and calculate labels per year
+### 2) Read yearly strata raster and extract values to the SpatialPoints object (i.e. shapefile) 
+### 3) Calculate reference class area proportions and variance of ref samples per strata
+### 4) Calculate unbiased standard error for proportion of reference class areas 
 
 require(rgdal)
 require(raster)
 
-## READ SHAPEFILE AND CALCULATE REFERENCE LABELS PER YEAR
+## 1) READ SHAPEFILE AND CALCULATE REFERENCE LABELS PER YEAR
+# This section takes the sample shapefile and assigns the proper strata for each year, depending
+# on when the model break happened. For now it's written to filter the pixels with only ONE CHANGE (1020 pts) 
 
+# Read shapefile with reference strata and change info
 setwd("C:/OneDrive/Lab/sample_may2016/interpreted_w_strata_23062016")
-
-##  CALCULATE UNBIASED AREA PROPORTIONSfull_samples <- readOGR(".", "final_extended_sample_merge_UTM18N_point")
+full_samples <- readOGR(".", "final_extended_sample_merge_UTM18N_point")
 
 # Subset to only use records with one or no change
 samples <- full_samples[full_samples@data$NUMCHANGES <= 1, ]
-
-
-# Split with anything that is not a number, not used here bc is inside the loop for now
-#a <- na.omit(as.numeric(unlist(strsplit(samples$CHGDATE, "[^0-9]"))))
 
 # We need to convert the dates to characters bc they are factors right now
 samples$CHGDATE <- as.character(samples$CHGDATE)
@@ -61,34 +60,34 @@ names(df) = field_names
 samples@data[,field_names] <- df
 #writeOGR(samples, "sample_yearly_strata", "sample_yearly_strata", driver="ESRI Shapefile", overwrite_layer = T)
 
-## READ YEARLY STRATA RASTERS AND EXTRACT THEIR VALUES TO THE SHAPEFILE
+## 2) READ YEARLY STRATA RASTERS AND EXTRACT THEIR VALUES TO THE SHAPEFILE
 # Create a list with the raster names
 years_short = seq(02,16)
-rast_names = paste0("final_strata_01_", sprintf("%02d",years_short), "_UTM18N.tif")
+rast_names = paste0("final_strata_01_", sprintf("%02d",years_short), "_UTM18N")
 
 # Iterate over names and extract to shapefile
 for (r in rast_names){
-  map = raster(paste0("C:/test/", r))
+  map = raster(paste0("C:/test/", r, ".tif"))
   samples = extract(map, samples, sp = TRUE) 
 }
 
-# Crosstab reference sample vs final strata (DOESN'T MATCH EXCEL FILE BC THE RECORDS WERE FILTERED ABOVE. If run with original, confusion
-# matrices are the same)
+# Crosstab reference sample vs final strata (DOESN'T MATCH EXCEL FILE BC THE RECORDS WERE FILTERED ABOVE. 
+# If run with original, confusion matrices are the same)
 ct = table(samples$final_strata_01_16_UTM18N, samples$strata)
 
 # LOAD the total strata sample size produced from CountValues.py, bc calculating it here with hist() takes forever...
 ss = read.csv("C:/test/strata_count.csv", header=TRUE, col.names=c("stratum", "pixels"))
-# Classes to be removed
-cs = c(7, 10, 12, 15)
+# Classes to be removed/ignored
+cr = c(7, 10, 12, 15)
 # Filter classes NOT in that list
-ss = ss[!(ss$stratum %in% cs),] 
+ss = ss[!(ss$stratum %in% cr),] 
 
 # Calculate total number of samples per ORIGINAL stratum
 strata_pixels = aggregate(samples$final_strata_01_16_UTM18N, by=list(samples$final_strata_01_16_UTM18N), length)
 
-## Calculate reference class area proportions and variance of ref samples per strata. 
-## Returns a vector with length equal to the number of reference classes
-## Reorganize to make more legible
+## 3) CALCULATE REFERENCE CLASS AREA PROPORTIONS AND VARIANCE OF REFERENCE SAMPLES PER STRATA
+# Returns a vector with length equal to the number of reference classes
+# Reorganize to make more legible
 
 calc_area_prop = function(strata, reference){
 
@@ -99,6 +98,8 @@ calc_area_prop = function(strata, reference){
   # Obtain unique values in the STRATA field
   str_codes = sort(unique(strata))
   str_ind=seq_along(str_codes)
+  
+  # Check if values in strata are the same as in reference, or check i
   
   # Initialize empty df for proportions per strata, and for sample variance per strata
   ref_prop = data.frame()
@@ -116,9 +117,10 @@ calc_area_prop = function(strata, reference){
       # Get proportion on reference class present on that strata
       ref_prop[s, r] = length(ind)/strata_pixels$x[strata_pixels$Group.1 == str_codes[s]]
       # Calculate variance of current reference code in current strata (needed later)
+      # THIS IS PRODUCING NA'S IN 01-02, REF-2002 IN STRATA 8 BC THERE IS ONLY ONE OBSERVATION, HOW TO DEAL
+      # WITH THIS?
       ref_var[s, r] = var(cond_bool[str_ref])
     }
-  
   }
   
   # Assign column and row names for easier reference
@@ -127,40 +129,66 @@ calc_area_prop = function(strata, reference){
   rownames(ref_var) = paste0("strat_",str_codes)
   colnames(ref_var) = paste0("ref_", ref_codes)
   
-  # Calculate reference class proportions (i.e. by columns) using total, original strata areas. Vectorize to avoid the ugly loop
+  # Calculate reference class proportions (i.e. by columns) using total, original strata areas.
   class_prop = vector()
+  # Filter only total sample sizes that are present in the strata for that year
+  fss = ss[ss$stratum %in% str_codes,]
   for (r in 1:ncol(ref_prop)){
-    class_prop[r] = sum(ss$pixels * ref_prop[,r])/sum(ss$pixels)
+    # LEAVE THE SUM OF THE ENTIRE STRATA?
+    class_prop[r] = sum(fss$pixels * ref_prop[,r])/sum(ss$pixels)
   }
-  return(list(class_prop, ref_var)) 
+  return(list(class_prop, ref_var, fss, ref_prop)) 
+  #return(ref_prop)
 }
 
 # Call the function for every reference year we want and get area proportions and sample variance
 # NOTE the double square brackets to allow for substitution
-ca = calc_area_prop(samples$final_strata_01_16_UTM18N, samples$ref_2001)
-cp = ca[[1]]
-rvar = ca[[2]]
+ca = calc_area_prop(samples$final_strata_01_02_UTM18N, samples$ref_2002)
 
 area_prop = data.frame()
 ref_var_list = list()
-for (i in (1:length(field_names))){
-  out = calc_area_prop(samples$final_strata_01_16_UTM18N, samples[[field_names[i]]]) #ALSO ITERATE OVER MULTI YEAR STRATA!?!?
+filtered_ss = list()
+for (i in (1:length(rast_names))){
+  # Compare year strata with year reference. Field names must start at 2002, hence i+1
+  out = calc_area_prop(samples[[rast_names[i]]], samples[[field_names[i+1]]]) 
   ap = out[[1]]
   rv = out[[2]]
+  fs = out[[3]]
   area_prop = rbind(area_prop, ap)
   ref_var_list[[i]] = rv
+  filtered_ss[[i]] = fs
 }
 
 # Assign names to make it easier to interprete
-rownames(area_prop) = years
-colnames(area_prop) = ref_codes
+rownames(area_prop) = years[2:length(years)]
+refcodes = sort(unique(samples$ref_2016))
+colnames(area_prop) = refcodes
 #No all the rows in area_prop add to 1, check!
 
-## CALCULATE UNBIASED STANDARD ERROR FOR PROPORTION OF REFERENCE CLASS AREAS
-function to iterate overeach class, then call the fnc for each year
-N=sum(ss$pixels)
-v = 1/N^2 * (sum(ss$pixels^2 * (1 - strata_pixels$x/ss$pixels) * (ref_var_list[[1]][,1] / strata_pixels$x)))
-se = sqrt(v)
+## 4) CALCULATE UNBIASED STANDARD ERROR FOR PROPORTION OF REFERENCE CLASS AREAS
+# add function to iterate over each class, then call the fnc for each year until 2015
+# get filtered sample size from filtered_ss for each year
 
-# To check which numbers on a list are part of others (x %in% y). To "merge" two lists of numbers, in case it's needed
-# to complete the list of available classes union(x,y)
+se_prop = data.frame()
+#Iterate over years
+for (y in 1:length(ref_var_list)){
+  # Iterate over classes
+  for (c in 1:length(refcodes)){
+  N=sum(ss$pixels)
+  v = 1/N^2 * (sum(ss$pixels^2 * (1 - strata_pixels$x/ss$pixels) * (ref_var_list[[y]][,c] / strata_pixels$x)))
+  se_prop[y,c] = sqrt(v)
+  }
+}
+
+# Reassign names...
+rownames(se_prop) = years[2:length(years)]
+colnames(se_prop) = refcodes
+## Years 2002 and 2010 have NA's because stratum 8 and 13 (respectively) only have 1 value and thus no variance
+
+# Calculate area in ha from area proportions 
+area_ha = area_prop*N * 30^2 / 100^2
+# Calculate confidence interval
+area_ci = se_prop * 1.96 * N
+#Upper and lower CI
+area_ha + area_ci
+area_ha + area_ci
