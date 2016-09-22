3b72791cb01bd0121b6bb1c4110b9030f44017b4
parevalo <parevalo@bu.edu>
2016-09-22 18:32:46 -0400

    Fix gdalwarp and add condition to select raster width depending on year

--- multi_scene/other/IDEAM_processing.sh
+++ multi_scene/other/IDEAM_processing.sh
@@ -26,74 +26,89 @@ period_list="03-05" #05-07 07-09 09-10 11-13 13-14 14-15" #03-05
 # 6 Stable regrowth, 7 Anything to regrowth (regrowth), 8 Regrowth to others 
 # 9 others to forest (regrowth)
 for p in $period_list; do
-    f="custom__strata_"$p"_UTM18N.tif"
+    f="custom_strata_"$p"_UTM18N.tif"
     
     # 1) Create custom strata 
     y1=${p:0:2}
     y2=${p:3:2}
-    qsub -j y -V -N strata_$p -b y \
-     gdal_calc.py -A ../20$y1"_final_crop.tif" -B ../20$y2"_final_crop.tif" \
-      --outfile=$f \
-      --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*5+' \
-              'logical_and(A == 3, B==3)*5 + logical_and(A == 4, B==4)*5 +' \
-              'logical_and(A == 5, B==5)*6 + logical_and(A == 5, B==1)*4+' \
-              'logical_and(A == 6, B==6)*5 +' \
-              'logical_and(A == 7, B==7)*5 + logical_and(A == 1, B==4)*2 +' \
-              'logical_and(A == 1, B==5)*3 +' \
-              'logical_and(A == 1, logical_or(B==2, B==3))*2 +' \
-              'logical_and(A == 1, logical_or(B==6, B==7))*2 +' \
-              'logical_and(A == 1, B == 0)*2 +' \
-              'logical_and(A == 4, B==5)*7 +' \
-              'logical_and(logical_or(A == 2, A==3), B==5)*7 +' \
-              'logical_and(logical_or(A == 6, A==7), B==5)*7 +' \
-              'logical_and(logical_and(logical_and(A!=0,A!=1), A!=5), B==0)*5 +' \
-              'logical_and(A == 5, logical_and(B != 5, B!= 1))*8 +' \
-              'logical_and(logical_or(A == 2, A==3), B==1)*9 +' \
-              'logical_and(logical_or(A == 4, A==6), B==1)*9 +' \
-              'logical_and(A == 7, B==1)*9 +' \
-              'logical_and(A == 0, B==0)*15"' \
-      --type=Byte --co="COMPRESS=PACKBITS" --overwrite
+#    qsub -j y -V -N strata_$p -b y \
+#     gdal_calc.py -A ../20$y1"_final_crop.tif" -B ../20$y2"_final_crop.tif" \
+#      --outfile=$f \
+#      --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*5+' \
+#              'logical_and(A == 3, B==3)*5 + logical_and(A == 4, B==4)*5 +' \
+#              'logical_and(A == 5, B==5)*6 + logical_and(A == 5, B==1)*4+' \
+#              'logical_and(A == 6, B==6)*5 +' \
+#              'logical_and(A == 7, B==7)*5 + logical_and(A == 1, B==4)*2 +' \
+#              'logical_and(A == 1, B==5)*3 +' \
+#              'logical_and(A == 1, logical_or(B==2, B==3))*2 +' \
+#              'logical_and(A == 1, logical_or(B==6, B==7))*2 +' \
+#              'logical_and(A == 1, B == 0)*2 +' \
+#              'logical_and(A == 4, B==5)*7 +' \
+#              'logical_and(logical_or(A == 2, A==3), B==5)*7 +' \
+#              'logical_and(logical_or(A == 6, A==7), B==5)*7 +' \
+#              'logical_and(logical_and(logical_and(A!=0,A!=1), A!=5), B==0)*5 +' \
+#              'logical_and(A == 5, logical_and(B != 5, B!= 1))*8 +' \
+#              'logical_and(logical_or(A == 2, A==3), B==1)*9 +' \
+#              'logical_and(logical_or(A == 4, A==6), B==1)*9 +' \
+#              'logical_and(A == 7, B==1)*9 +' \
+#              'logical_and(A == 0, B==0)*15"' \
+#      --type=Byte --co="COMPRESS=PACKBITS" --overwrite
+#
+#    # 2) Reproject to MAGNA-SIRGAS and grid
+#    # This section works fine except because the output file has a 2 meter 
+#    # offset in the lower left Y coordinate for an unknown reason. Also, the 
+#    # annual IDEAM files have a sligtly different origin than the others
+#
+#
+#    # Use 15 as input and output nodata, because the "other to other" class is 
+#    # still labeled as zero. User -ts instead of -tr because that was creating
+     # a displacement in the grid for some reason. IDEAM annual maps have a height
+     # of 61620 though
 
-    # 2) Reproject to MAGNA-SIRGAS and grid
-    # This section works fine except because the output file has a 2 meter 
-    # offset in the lower left Y coordinate for an unknown reason. Also, the 
-    # annual IDEAM files have a sligtly different origin than the others
+    if [ $p == 13-14 ] || [ $p == 14-15 ] ;then                                 
+        height=61620                                                       
+    else                                                                        
+        height=61618                                                     
+    fi         
 
-
-    # Use 15 as input and output nodata, because the "other to other" class is 
-    # still labeled as zero
     qsub -V -b y -j y -N reproj_$p -hold_jid strata_$p \
      gdalwarp -co COMPRESS=PACKBITS -co NBITS=4 -wt Byte \
       -te $(gdal_extent cambio_2002_2004_v6_amazonia.tif) -te_srs EPSG:3116 \
-       -t_srs EPSG:3116 -tr 30.717 -30.2624 -srcnodata 15 -dstnodata 15 \
+       -t_srs EPSG:3116 -ts 45206 $heigth -srcnodata 15 -dstnodata 15 \
         -overwrite $f $(basename $f .tif)"_MAGNA.tif"
-    
-
-    # 3) Sieve "polygons" of less than 1 ha (around 11.11 pixels)
-    qsub -V -b y -j y -N sieve_$p -hold_jid reproj_$p  gdal_sieve.py -st 11 -8 \
-     $(basename $f .tif)"_MAGNA.tif" $(basename $f .tif)"_MAGNA_sieved.tif"
-
-
-    # 4) Use gdalcalc to compare the the maps
-    # Revert to IDEAM's numbering
-    y1p=`expr $y1 - 1`
-    y2p=`expr $y2 - 1`
-    printf -v yr1 '%02d' $y1p 
-    printf -v yr2 '%02d' $y2p
-
-    #IDEAM labels: 1 forest, 2 defor, 3 no info, 4 regrowth, 5 stable non-forest        
-    qsub -j y -V -N compare_$p -hold_jid sieve_$p -b y \
-     gdal_calc.py -A "cambio_20"$yr1"_20"$yr2"_v6_amazonia.tif" \
-      -B $(basename $f .tif)"_MAGNA_sieved.tif" \
-      --outfile=comparison_$p \
-       --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*2+' \
-               'logical_and(A == 4, logical_or(B==3, B==4 )*4 +' \
-               'logical_and(A == 4, logical_or(B==6, B==7 )*4 +' \
-               'logical_and(A == 4, B==9 )*4 +' \
-               'logical_and(A == 5, B==5 )*5"' \
-       --type=Byte --co="COMPRESS=PACKBITS" --overwrite
-
+#
+#    # 3) Sieve "polygons" of less than 1 ha (around 11.11 pixels)
+#    qsub -V -b y -j y -N sieve_$p -hold_jid reproj_$p  gdal_sieve.py -st 11 -8 \
+#     $(basename $f .tif)"_MAGNA.tif" $(basename $f .tif)"_MAGNA_sieved.tif"
+#
+#
+#    # 4) Use gdalcalc to compare the the maps
+#    # Revert to IDEAM's numbering
+#    y1p=`expr $y1 - 1`
+#    y2p=`expr $y2 - 1`
+#    printf -v yr1 '%02d' $y1p 
+#    printf -v yr2 '%02d' $y2p
+#
+#    #IDEAM labels: 1 forest, 2 defor, 3 no info, 4 regrowth, 5 stable non-forest        
+#    qsub -j y -V -N compare_$p -hold_jid sieve_$p -b y \
+#     gdal_calc.py -A "cambio_20"$yr1"_20"$yr2"_v6_amazonia.tif" \
+#      -B $(basename $f .tif)"_MAGNA_sieved.tif" \
+#      --outfile=comparison_$p".tif" \
+#       --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*2+' \
+#               'logical_and(A == 4, logical_or(B==3, B==4 ))*4 +' \
+#               'logical_and(A == 4, logical_or(B==6, B==7 ))*4 +' \
+#               'logical_and(A == 4, B==9 )*4 +' \
+#               'logical_and(A == 5, B==5 )*5 +' \
+#               'logical_and(A == 2, B==1 )*6 +' \
+#               'logical_and(A == 2, logical_or(B==3, B==8 ))*7 +' \
+#               'logical_and(A == 5, B==1 )*8 +' \
+#               'logical_and(A == 5, B==6 )*9 +' \
+#               'logical_or(A == 0 , B==15 )*14"' \
+#       --type=Byte --NoDataValue=15 --co="COMPRESS=PACKBITS" --co NBITS=4 --overwrite
+#
 #1 forest agreem, 2 defor agreem, 4 regrowth agreem, 5 nonforest agreem, 
-
+#6 IDEAM defor - strata forest, 7 IDEAM-defor - strata- 3 and 8 ("loss" of forest)
+#8 IDEAM NF - strata forest, 9 IDEAM NF -strata stable regrowth.
+#15 added nodata areas
 done
 
