d6e72d9dc8c926386cf59fa73ec8cff9459ccec2
parevalo <Parsif4l>
2016-12-05 16:47:55 -0500

    Add code to create strata by year instead of cumulatively

--- multi_scene/6_stratification/2_create_strata.sh
+++ multi_scene/6_stratification/2_create_strata.sh
@@ -2,16 +2,46 @@
 
 # Script to create the strata from mosaics
 
+# CD to folder and define suffix of input tif files
+
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
+suffix="_final_crop.tif"
 
-initial=2001_final_crop.tif
 
-# Get strata for each year we might want to check 
+# Define the start and end year we want calculate the strata on
 
-for yr in $(seq -w 16 16); do
+first_yr=1
+last_yr=16
+
+# Define if we want the strata to be annual or cumulative (e.g. 2001 to 2005)
+# and set variables accordingly. Make sure they are zero padded
+
+annual=true
+fy=$(printf %02d $first_yr)
+
+if [ $annual = true ]; then
+    ly=$(printf %02d `expr $last_yr - 1`)      
+else
+    ly=$(printf %02d $last_yr)
+fi
+
+# Create the strata following those parameters
+
+for yr in $(seq -w $fy $ly); do
+    if [ $annual = true ]; then
+        first=20$yr$suffix
+        yr2=$(printf %02d `expr $yr + 1`)
+        second=20$yr2$suffix
+        outfile="final_strata_annual_"$yr"_"$yr2"_UTM18N.tif"
+    else
+        first=20$fy$suffix
+        second=20$yr$suffix
+        outfile="final_strata_"$fy"_"$yr"_UTM18N.tif"
+    fi
+    
     qsub -j y -V -N strata_$yr -b y \
-     gdal_calc.py -A $initial -B 20$yr"_final_crop.tif" \
-      --outfile=final_strata_01_$yr"_UTM18N.tif" \
+     gdal_calc.py -A $first -B $second \
+      --outfile=$outfile \
       --calc='"logical_and(A == 1, B==1)*1 + logical_and(A == 2, B==2)*2 +' \
               'logical_and(A == 3, B==3)*3 + logical_and(A == 4, B==4)*4 +' \
               'logical_and(A == 5, B==5)*5 + logical_and(A == 5, B==1)*5 +' \
@@ -29,3 +59,4 @@ for yr in $(seq -w 16 16); do
               'logical_and(A == 0, B==0)*15"' \
       --type=Byte --co="COMPRESS=PACKBITS" --overwrite
 done
+
