010f55944535f7de2dc3b7015d401d39df0aea9e
Paulo Arevalo <parevalo@bu.edu>
2017-04-03 10:00:16 -0400

    New input files for buffered strata from forest to pastures only

---
+++ multi_scene/7_poststratification/input_variables_original_buffer1B.R
@@ -0,0 +1,28 @@
+# File with path to necessary files to run the area estimation analysis with the original parameters
+
+# Paths and suffixes/prefixes
+savepath = "/media/paulo/785044BD504483BA/OneDrive/Lab/area_calculation/results/original_buffer1B/"
+lutpath = "/home/paulo/workflow/multi_scene/data/original_lut.csv"
+lut_name = "original_lut"
+orig_stratif = "buffered1B_final_strata_01_16_UTM18N"
+rast_prefix = "final_strata_annual_"
+rast_suffix = "_UTM18N"
+pixcount_suffix = "_pixcount.csv"
+pixcount_strata = paste0("buffered1B_strata_01_16", pixcount_suffix)
+
+# Reference strata names (Ordered names of the classes that will get their areas estimated based on the reference samples)
+strata_names = c("Other to other", "Stable forest", "Stable grassland", "Stable Urban + Stable other", 
+                 "Stable pasture-cropland", "Stable secondary forest", "Stable water", "Forest to pasture", 
+                 "Forest to secondary forest", "Gain of secondary forest", "Loss of secondary forest")
+
+# List of original strata names (Ordered names of all the classes from the original stratification, including those for which
+# areas won't be estimated)
+orig_strata_names = c("Other to other", "Stable forest", "Stable grassland", "Stable Urban + Stable other", 
+                      "Stable pasture-cropland", "Stable secondary forest", "Stable water", "Forest to pasture", 
+                      "Forest to secondary forest", "Gain of secondary forest", "All to unclassified", "Loss of secondary forest", "Buffer")
+
+cr = c(7, 10, 12, 15) # Classes to ignore from the loaded area count tables
+step = 1  # Number of years to do the analysis over
+
+
+
