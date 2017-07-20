2afd0a866709b13a78b0a51b7b8ebfa155c6bcf9
Paulo Arevalo <parevalo@bu.edu>
2017-07-20 18:26:22 -0400

    Calculate areas and accuracies for each period

--- multi_scene/6_stratification/5_biannual_sample_interpreted_processing.R
+++ multi_scene/6_stratification/5_biannual_sample_interpreted_processing.R
@@ -26,18 +26,25 @@ csv_names = character()
 shp_list = list()
 csv_list = list()
 short_years = substr(years, 3,4) # Get years in two digit format
+pixcount_list = list()
 
 for (y in 1:(length(years)-1)){
   fname = paste0("sample_", short_years[y], "_", short_years[y+1])
 
-  # Load shapes
+  # Load shapes with map strata
   samples_names[y] = fname
   shp_list[[y]] = readOGR(paste0(auxpath, "biannual_samples/"), samples_names[y])
 
-  #Load csvs
+  # Load csvs with reference strata
   csv_list[[y]] <- read.csv(paste0(samples_names[y], ".csv"))
+  
+  # Load csvs with map pixel count
+  fname = paste0("strata_buffered_", short_years[y], "_", short_years[y+1], "_pixcount.csv")
+  pixcount_list[[y]] = read.csv(paste0(auxpath,"biannual_samples/", fname), header=TRUE, col.names=c("stratum", "pixels"))  
+  pixcount_list[[y]] = pixcount_list[[y]][!(pixcount_list[[y]]$stratum %in% cr),]     
 
 }
+
 
 # Get row count per sample file to verify
 samples_nrows = as.data.frame(do.call(rbind, lapply(csv_list, nrow)))
@@ -70,18 +77,62 @@ join_ref_map_strata = function(map_shp, refstrata_id){
 # Use mapply to use each corresponding element of the two lists
 shp_list_ref = mapply(join_ref_map_strata, shp_list, ref_strata)
 
+# Get lists of ref and map codes per period. This can probably be simplified
+get_ref_codes = function(shp){
+  refcodes = sort(unique(shp@data$ref_strata))
+  return(refcodes)
+}
+
+get_map_codes = function(shp){
+  mapcodes = sort(unique(shp@data$STRATUM))
+  return(mapcodes)
+}
+
+ref_codes = lapply(shp_list_ref, get_ref_codes)
+map_codes = lapply(shp_list_ref, get_map_codes)
+
+
 # Crosstab map and ref labels for each period
-create_cm = function(shape){
-  ref_codes = sort(unique(shape@data$ref_strata))
-  map_codes = sort(unique(shape@data$STRATUM))
-  class_codes = sort(union(ref_codes, map_codes))
+create_cm = function(shape, refcodes, mapcodes){
+  class_codes = sort(union(refcodes, mapcodes))
   cm = calc_ct(shape@data$STRATUM, shape@data$ref_strata, class_codes)
   return(cm)
 }
 
-cm_list = lapply(shp_list_ref, create_cm)
+cm_list = mapply(create_cm, shp_list_ref, ref_codes, map_codes, SIMPLIFY = F)
 
+# Check that the sample allocation is correct
+map_sample_count = as.data.frame(do.call(rbind, lapply(cm_list, rowSums)))
 
+# Check all the maps have the same total area in pixels and use that value
+map_total_pix = as.data.frame(do.call(rbind, lapply(pixcount_list, colSums)))
+tot_area_pix = map_total_pix[1,2]
 
+# Create single variable with sample allocation
+strata_pixels = aggregate(shp_list[[1]]$STRATUM, by=list(shp_list[[1]]$STRATUM), length)
+
+# Calculate areas and accuracies, cant vectorize it the way the fncs are written
+prop_out = list()
+se_prop = list()
+areas_out = list()
+accuracies_out = list()
+
+# Should I use same ref_codes for all?
+for (p in 1:length(csv_list)){
+  prop_out[[p]] = calc_props_and_vars(shp_list_ref[[p]]$STRATUM, shp_list_ref[[p]]$ref_strata, shp_list[[p]]$STRATUM, 
+                                  pixcount_list[[p]], strata_pixels, ref_codes[[p]])
+  
+  se_prop[[p]] = calc_se_prop(pixcount_list[[p]], strata_pixels, prop_out[[p]][[2]], ref_codes[[p]], tot_area_pix)
+  areas_out[[p]] = calc_unbiased_area(tot_area_pix, prop_out[[p]][[11]], se_prop[[p]]) 
+  accuracies_out[[p]] = calc_accuracies(pixcount_list[[p]], strata_pixels, ref_codes[[p]], tot_area_pix,
+                                    prop_out[[p]][[1]], prop_out[[p]][[2]], prop_out[[p]][[3]], prop_out[[p]][[4]],
+                                    prop_out[[p]][[5]], prop_out[[p]][[6]], 
+                                    prop_out[[p]][[7]], prop_out[[p]][[8]],
+                                    prop_out[[p]][[9]], prop_out[[p]][[10]])
+
+}
+
+# Get them in a readable format!
+lapply(accuracies_out, '[[', 1)
 
 
