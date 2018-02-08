d154804c3d6d8deb6cd0a04156d4828d024eb2ef
Paulo Arevalo <parevalo@bu.edu>
2018-02-08 17:12:18 -0500

    Convert code to functions and run for three different values of idp

--- multi_scene/other/interpolation_test.R
+++ multi_scene/other/interpolation_test.R
@@ -84,37 +84,19 @@ create_partitions = function(group_vect, id_vect, npart,fraction){
   return(class_folds)
 }
 
+
+#' Fnc to get CV results per single period, all classes
 #' Iterate over classes and number of neighbors in a pseudo CV fashion to obtain
 #' the NN we will use.
 #' If there is only one ocurrence of a value, we need the sample_frac to be >=0.6 
 #' so that the single occurrence gets included in every sample we take. 
+#' @param class_list: List of classes to iterate over
+#' @param full_shp: Shapefile with all the samples
+#' @param npart: number of data partitions to create
+#' @param fraction: fraction to use for the stratified partition
+#' @param nb_seq: sequence of neighbors to run the interpolation over
+#' @param ipd_value: Value of idp used for the interpolation
 
-nsamples = 10
-nb = seq(from = 5, to = 30 , by = 1)
-sample_frac = 0.6
-idp = 1.5
-cv_results = list()
-class_results = list()
-shp_results = list()
-crs_obj = CRS("+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
-set.seed(1512)
-
-# Run for a single period-class: (e.g. 09-11, #5)
-
-# class_results = list()
-# for (i in 1:length(ref_class_list[[5]])){
-#   print(paste0("Iterating with map class ", ref_class_list[[5]][[i]]))
-#   subset_ds = shp_list[[5]][shp_list[[5]]$map_strata == ref_class_list[[5]][[i]],]
-#   test_part = create_partitions(subset_ds@data$binmatch, subset_ds@data$ID, 10, 0.6)
-#   
-#   # Run with multiple nearest neighbors values
-#   cv_results = mclapply(nb, run_cv, fold_list=test_part, input_data=subset_ds, idp_val=idp)
-# 
-#   names(cv_results) = sprintf("%02d",nb)
-#   class_results[[i]] = cv_results
-# }
-
-# Fnc to get CV results per single period, all classes
 get_class_cv_results = function(class_list, full_shp, npart, fraction, nb_seq, idp_val){
   out_class_results = list()
   for (i in 1:length(class_list)){
@@ -131,34 +113,14 @@ get_class_cv_results = function(class_list, full_shp, npart, fraction, nb_seq, i
   return(out_class_results)
 }
 
-class_results1 = get_class_cv_results(ref_class_list[[5]][[4]], shp_list[[5]], nsamples, sample_frac, nb, idp)
 
-# Store AUC results per class/partition/neighbor in lists and create boxplots with them.
-# Also select highest AUC automatically to input into the interpolation
-# Done here for period 09-11
+#' Fnc to calculate the # of neighbors per class required to do the interpolation
+#' based on the highest mean AUCROC calculated from the CV samples. 
+#' @param class_list: List of classes to iterate over
+#' @param class_results: Results of running the CV in the previous step
+#' @param ipd_value: Value of IDP used to obtain the results. Only used here for
+#' plot labels and filenames.
 
-class_auc = list()
-auc_boxplot = list()
-mean_auc = list()
-max_nn = list()
-for (i in 1:length(ref_class_list[[5]])){
-  
-  # Extract AUCs per partition per neighbor
-  auc_list_nb = as.data.frame(do.call(cbind, lapply(class_results[[i]], function(x){x[[2]]})))
-  class_auc[[i]] = gather(auc_list_nb, neigh, auc)
-  auc_boxplot[[i]] <- ggplot(class_auc[[i]], aes(x = neigh, y = auc)) + geom_boxplot() + 
-    ylab("AUCROC") + xlab("# Neighbors") + ggtitle(paste0("idp=", idp))
-  ggsave(paste0("interpolation/auc_vs_nn_class", ref_class_list[[5]][[i]], "_idp", idp, ".png"),
-         plot=auc_boxplot[[i]], width = 10, height = 5, units='in') 
-  
-  # Select highest mean AUC
-  mean_auc[[i]] = class_auc[[i]] %>% group_by(neigh) %>% summarise(avg=mean(auc))
-  # Rewrite to make it easier to read. But the idea is that if two numbers meet 
-  # the criteria, take the smaller neighbor
-  max_nn[[i]] = as.numeric(min(mean_auc[[i]]$neigh[mean_auc[[i]]$avg == max(mean_auc[[i]]$avg)]))
-}
-
-# Fnc to calculate the # of neighbors per class required to do the interpolation
 calc_max_nn = function(class_list, class_results, idp_val){
   class_auc = list()
   auc_boxplot = list()
@@ -183,16 +145,10 @@ calc_max_nn = function(class_list, class_results, idp_val){
   return(max_nn)
 }
 
-max_nn_idp15 = calc_max_nn(ref_class_list[[5]], class_results1, idp)
-
-# Load rasters where interpolation will be performed
-testraster = raster("/media/paulo/785044BD504483BA/test/final_strata_annual_09_11_UTM18N.tif")
-# Crop to be able to test locally
-cropped_raster = crop(testraster, y=extent(testraster, 10000, 14000, 10000, 14000))
-NAvalue(cropped_raster) = 15
 
 #' Function to run interpolation over a given raster for a list of classes, 
 #' given a training ds and idp.
+
 interpolate_fnc = function(cat_raster, train_shp, class_list, nn, idp_val, nsuffix){
   # Needed to convert to SpatialPixels
   crs_string ="+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
@@ -213,61 +169,112 @@ interpolate_fnc = function(cat_raster, train_shp, class_list, nn, idp_val, nsuff
                     newdata=area_sp, idp=idp_val, nmax=nn[[i]])
     class_interp = raster(class_idw["var1.pred"])
     
-    rast_name = paste0("interpolation/IDW_class_", class_list[[i]], "_nn", max_nn[[i]], 
+    rast_name = paste0("interpolation/IDW_class_", class_list[[i]], "_nn", nn[[i]], 
                        "_idp", idp_val, nsuffix)
     
     writeRaster(class_interp, rast_name, format="GTiff", overwrite=TRUE)
   }
 }
 
-# In order to test the performace of the definitive interpolation, set aside
-# training and testing datasets. 
-train_part = list()
-train_ds = list()
-test_ds = list()
-id_list = list()
-
-# Get fractional samples per class
-for (i in 1:length(ref_class_list[[5]])){
-  classds = shp_list[[5]][shp_list[[5]]$map_strata == ref_class_list[[5]][[i]],]
-  # Ensure we can get the same samples every time
-  set.seed(1515)
-  train_part[i] = create_partitions(classds@data$binmatch, classds@data$ID, 1, 0.6)
-  # get ID of the rows and then subset them all at once 
-  id_list[[i]] = classds$ID[train_part[[i]]]
+#' Function to create a test and training sets from the initial sample shapefile
+#' @param class_list: List of classes to iterate over
+#' @param full_shp: Shapefile with all the samples
+create_test_train_ds = function(class_list, full_shp){
+  
+  train_part = list()
+  id_list = list()
+  
+  # Get fractional samples per class
+  for (i in 1:length(class_list)){
+    classds = full_shp[full_shp$map_strata == class_list[[i]],]
+    train_part[i] = create_partitions(classds@data$binmatch, classds@data$ID, 1, 0.6)
+    # get ID of the rows and then subset them all at once 
+    id_list[[i]] = classds$ID[train_part[[i]]]
+  }
+  
+  # Unlist and find boolean indices for train and test datasets. 
+  train_ids = unlist(id_list)
+  train_sel = full_shp$ID %in% train_ids
+  out_train = full_shp[train_sel,] 
+  out_test = full_shp[!train_sel,] 
+  return(c(out_train, out_test, train_sel))
 }
 
-# Unlist and find boolean indices for train and test datasets. 
-train_ids = unlist(id_list)
-train_sel = shp_list[[5]]$ID %in% train_ids
-train_ds = shp_list[[5]][train_sel,] 
-test_ds = shp_list[[5]][!train_sel,] 
+
+# Fnc to load interpolated rasters to calculate AUCROC
+#' @param class_list: List of classes to iterate over
+#' @param nn: List with the definitive number of neighbors per class to use
+#' @param ipd_value: Value of idp used for the interpolation
+#' @param test_shp: Value with the test points used to calculate the final AUCROC
+calculate_final_aucroc = function(class_list, nn, idp_val, test_shp){
+  int_rasts = list()
+  crs_string ="+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+  
+  for (i in 1:length(class_list)){
+    rast_name = paste0("interpolation/IDW_class_", class_list[[i]], "_nn", nn[[i]], 
+                       "_idp", idp_val, "_train_09-11.tif")
+    int_rasts[[i]] = raster(rast_name)
+  }
+  
+  # Merge all rasters
+  interp_raster = do.call(raster::merge, int_rasts)
+  
+  # Extract values
+  # Need to convert to spatialpoints for extract to work correctly
+  sp_temp = SpatialPointsDataFrame(test_shp, proj4string =CRS(crs_string), data=test_shp@data)
+  sp_temp = raster::extract(interp_raster, sp_temp, sp = TRUE) 
+  
+  # Calculate ROC for final maps using the TEST dataset
+  interp_roc = roc(sp_temp$binmatch, sp_temp$layer)
+  return(interp_roc$auc)
+}
 
 
+############ RUN AND SAVE OUTPUTS FOR MULTIPLE VALUES OF IDP
+nsamples = 10
+nb = seq(from = 5, to = 30 , by = 1)
+sample_frac = 0.6
+
+# Create training and test dataset to be used with all the interpolations
+train_and_test = create_test_train_ds(ref_class_list[[5]], shp_list[[5]])
+train_ds = train_and_test[[1]]
+test_ds = train_and_test[[2]]
+
+# Load raster where interpolation will be performed
+testraster = raster("/media/paulo/785044BD504483BA/test/final_strata_annual_09_11_UTM18N.tif")
+# Crop to be able to test locally
+cropped_raster = crop(testraster, y=extent(testraster, 10000, 14000, 10000, 14000))
+NAvalue(cropped_raster) = 15
+
+# Run with IDP 0.5
+idp = 0.5
+set.seed(1512)
+class_results05 = get_class_cv_results(ref_class_list[[5]], shp_list[[5]], nsamples, sample_frac, nb, idp)
+max_nn_idp05 = calc_max_nn(ref_class_list[[5]], class_results05, idp)
 # Run with ALL points per class
-interpolate_fnc(cropped_raster, shp_list[[5]], ref_class_list[[5]], max_nn, idp, "_ALL_09-11.tif")
+#interpolate_fnc(cropped_raster, shp_list[[5]], ref_class_list[[5]], max_nn_idp05, idp, "_ALL_09-11.tif")
+# Run with TRAIN points per class only, only way to test final AUCROC
+interpolate_fnc(cropped_raster, train_ds, ref_class_list[[5]], max_nn_idp05, idp, "_train_09-11.tif")
+auroc05 = calculate_final_aucroc(ref_class_list[[5]], max_nn_idp05, idp, test_ds)
 
-# Run with TRAIN points per class only
-interpolate_fnc(cropped_raster, train_ds, ref_class_list[[5]], max_nn, idp, "_train_09-11.tif")
+# Run with IDP 1
+idp = 1
+set.seed(2487)
+class_results1 = get_class_cv_results(ref_class_list[[5]], shp_list[[5]], nsamples, sample_frac, nb, idp)
+max_nn_idp1 = calc_max_nn(ref_class_list[[5]], class_results1, idp)
+# Run with ALL points per class
+#interpolate_fnc(cropped_raster, shp_list[[5]], ref_class_list[[5]], max_nn_idp05, idp, "_ALL_09-11.tif")
+# Run with TRAIN points per class only, only way to test final AUCROC
+interpolate_fnc(cropped_raster, train_ds, ref_class_list[[5]], max_nn_idp1, idp, "_train_09-11.tif")
+auroc1 = calculate_final_aucroc(ref_class_list[[5]], max_nn_idp1, idp, test_ds)
 
-# Load interpolated rasters to calculate AUCROC
-testlist = list()
-int_rasts = list()
-
-for (i in 1:length(ref_class_list[[5]])){
-  rast_name = paste0("interpolation/IDW_class_", ref_class_list[[5]][[i]], "_nn", max_nn[[i]], 
-                     "_idp", idp, "_train_09-11.tif")
-  int_rasts[[i]] = raster(rast_name)
-}
-
-# Merge all rasters
-interp_raster = do.call(raster::merge, int_rasts)
-
-# Extract values
-# Need to convert to spatialpoints for extract to work correctly
-sp_temp = SpatialPointsDataFrame(test_ds, proj4string =crs_obj, data=test_ds@data)
-sp_temp = raster::extract(interp_raster, sp_temp, sp = TRUE) 
-
-# Calculate ROC for final maps using the TEST dataset
-interp_roc = roc(sp_temp$binmatch, sp_temp$layer)
-interp_roc$auc
+# Run with IDP 1.5
+idp = 1.5
+set.seed(9473)
+class_results15 = get_class_cv_results(ref_class_list[[5]], shp_list[[5]], nsamples, sample_frac, nb, idp)
+max_nn_idp15 = calc_max_nn(ref_class_list[[5]], class_results15, idp)
+# Run with ALL points per class
+#interpolate_fnc(cropped_raster, shp_list[[5]], ref_class_list[[5]], max_nn_idp05, idp, "_ALL_09-11.tif")
+# Run with TRAIN points per class only, only way to test final AUCROC
+interpolate_fnc(cropped_raster, train_ds, ref_class_list[[5]], max_nn_idp15, idp, "_train_09-11.tif")
+auroc15 = calculate_final_aucroc(ref_class_list[[5]], max_nn_idp15, idp, test_ds)
