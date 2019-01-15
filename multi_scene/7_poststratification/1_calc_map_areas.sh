c818e0a6e6c001302678160e2cac6ab556bb2708
parevalo <parevalo@bu.edu>
2019-01-15 12:39:56 -0500

    Minor changes

--- multi_scene/7_poststratification/1_calc_map_areas.sh
+++ multi_scene/7_poststratification/1_calc_map_areas.sh
@@ -5,14 +5,15 @@ spath=/projectnb/landsat/projects/Colombia/workflow/multi_scene/7_poststratifica
 
 # Find all strata maps with a given filename suffix
 cd /projectnb/landsat/projects/Colombia/Mosaics/M3
-matchstring='final_strata_annual*UTM18N.tif'
+matchstring='buffered3_final_strata_annual*UTM18N.tif'
+outprefix="strata_buffered_"
 outsuffix="_pixcount.csv"
 
 # Iterate over strata maps and get the year pair to use in the output names
 for i in $(ls $matchstring); do
    yr=$(ls $i | grep -Eo "[0-9]{2}_[0-9]{2}")
     qsub -j y -b y -V -N str_area_$yr -l mem_per_core=8G\
-     $spath/count_pixels.py $i "strata_"$yr$outsuffix
+     $spath/count_pixels.py $i $outprefix$yr$outsuffix
 
 done
 
